41876716691f96faf4b5c8d73e376cbfee2b913c
mshan10 <mshan@nd.edu>
2018-04-23 18:07:38 -0400

    finished lesson 32

---
+++ 32-transition-hooks/js/components/home/home.component.js
@@ -0,0 +1,25 @@
+var home = {
+	template: `
+		<div class="home">Home Page</div>
+	`
+};
+
+angular
+	.module('home')
+	.component('home', home)
+	.config(function ($stateProvider, $urlRouterProvider, $transitionsProvider) {
+		$transitionsProvider.onStart({
+			to: function (state) {
+				return !!(state.data && state.data.requiredAuth);
+			}
+		}, function ($transition$) {
+			console.log($transition$);
+		});
+
+		$stateProvider
+			.state('home', {
+				url: '/',
+				component: 'home'
+			});
+		$urlRouterProvider.otherwise('/');
+	});
